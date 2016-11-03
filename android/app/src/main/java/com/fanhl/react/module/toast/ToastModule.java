6787c5ba6b8a8bf7a27cb235dbead1b35256a15c
fanhl <fanhonglin@klicen.com>
2016-11-03 09:39:09 +0800

    Init commit

---
+++ android/app/src/main/java/com/fanhl/react/module/toast/ToastModule.java
@@ -0,0 +1,40 @@
+package com.fanhl.react.module.toast;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+/**
+ * 练习用的 Android端的Toast
+ *
+ * Created by fanhl on 2016/11/2.
+ */
+public class ToastModule extends ReactContextBaseJavaModule {
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastAndroid";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
+}
